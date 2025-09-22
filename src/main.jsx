c629d2407f5b5e04a038137337f94200fedc7746
yashdotdev <thisisyash459@gmail.com>
2025-09-22 17:57:10 +0530

    did some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from "./pages/Home/Home.jsx";
+import { Cart } from "./pages/Cart/Cart.jsx";
+import { CartProvider } from "./Context/CartContext.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      // {path: "/products", element: <Products/>},
+      // {path: "/products/:id", element: <ProductDetail/>},
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  </React.StrictMode>
 );
