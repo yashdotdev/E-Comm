1eac26b9cd54b357c006e450eb9d60a38736f6ed
yashdotdev <thisisyash459@gmail.com>
2025-09-23 11:12:14 +0530

    added new tailwind class

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import { FaStar } from "react-icons/fa";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="relative bg-white rounded-md shadow hover:shadow-lg transition p-4">
+    <div className="relative bg-white rounded-md shadow hover:shadow-lg transition p-4 cursor-pointer">
       {product.isHot && (
         <span className="absolute top-4 left-4 bg-red-500 text-white text-xs font-semibold px-5 py-2 r">
           HOT
@@ -31,7 +31,7 @@ const ProductCard = ({ product }) => {
               }`}
             />
           ))}
-   </div>
+        </div>
         <div className=" flex justify-center items-center text-center mt-2">
           <span className="text-sky-600 font-bold text-lg ">
             ${product.price}
