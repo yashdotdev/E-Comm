beb97d963314b95df1e35da9ecff985b88829e3c
yashdotdev <thisisyash459@gmail.com>
2025-09-24 11:11:19 +0530

    updated categories & wrapped the components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,25 +6,37 @@ import { ColorFilter } from "./ColorFilter";
 import { BrandFilter } from "./BrandFilter";
 
 export const Sidebar = () => {
-  const categories = ["nike", "airmax", "adidas", "vans", "all-stars"];
-  const categoryCounts = categories.reduce((acc, cat) => {
-    acc[cat] = products.filter((p) => p.category === cat).length;
-    return acc;
-  }, {});
+  const categories = ["nike", "airmax", "nike", "adidas", "vans", "all-stars", "adidas"];
+
+  const categoryItems = categories.map((category) => ({
+    name: category,
+    count: products.filter((product) => product.category === category).length,
+  }));
 
   const [showMoreCats, setShowMoreCats] = useState(false);
 
   return (
-    <div className="hidden lg:block bg-[#F6F7F8] lg:w-1/5 max-w-1/4 mt-4 ml-12 shadow-sm p-6 rounded-md overflow-y-auto max-h-screen">
-      <HotDeals
-        categories={categories}
-        categoryCounts={categoryCounts}
-        showMoreCats={showMoreCats}
-        setShowMoreCats={setShowMoreCats}
-      />
-      <PriceFilter />
-      <ColorFilter />
-      <BrandFilter />
+    <div className="hidden lg:block lg:w-1/5 max-w-1/4 mt-4 ml-12 overflow max-h-screen">
+      <div className="bg-[#F6F7F8] p-6 mb-4 rounded-md">
+        <HotDeals
+          categories={categories}
+          categoryCounts={categoryItems}
+          showMoreCats={showMoreCats}
+          setShowMoreCats={setShowMoreCats}
+        />
+      </div>
+
+      <div className="bg-[#F6F7F8] p-6 mb-4">
+        <PriceFilter />
+      </div>
+
+      <div className="bg-[#F6F7F8] p-6 mb-4">
+        <ColorFilter />
+      </div>
+
+      <div className="bg-[#F6F7F8] p-6 mb-4">
+        <BrandFilter />
+      </div>
     </div>
   );
 };
