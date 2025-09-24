1cbc2cc462ac5d5b76284fec0ae335e52aaf8ac6
yashdotdev <thisisyash459@gmail.com>
2025-09-24 11:08:52 +0530

    minor styling changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,11 @@ import HeroImage from "../../assets/Group 493.png";
 
 export const Hero = () => {
   return (
-    <div className="w-full mt-3 ">
+    <div className="w-full">
       <img
         src={HeroImage}
         alt="Hero"
-        className="w-full h-auto object-contain rounded-lg"
+        className="w-full h-auto object-contain"
       />
     </div>
   );
